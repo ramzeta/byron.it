431c9a816944a07def51eef97f54d34fdda242e0
ramzeta <ramiroperezsanz13@gmail.com>
2023-01-31 18:43:54 +0100

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,32 +5,13 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import byronchat from "../../Assets/Projects/byron-chat.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
-  return (
-    <Container fluid className="project-section">
-      <Particle />
-      <Container>
-        <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
-        </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/ramzeta"
-            />
-          </Col>
-
+  /**
+   * 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={bitsOfCode}
@@ -83,6 +64,28 @@ function Projects() {
               ghLink=""
             />
           </Col>
+   */
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={byronchat}
+              isBlog={false}
+              title="Byron Chat"
+              description="Chat with google authentication, with just one step you can start chatting. You can create your own room and customize it."
+              ghLink="https://github.com/ramzeta/byron-chat"
+            />
+          </Col>
+
         </Row>
       </Container>
     </Container>
