9e90886c3d41e873d364f21226aaca1760f226e1
ramzeta <ramiroperezsanz13@gmail.com>
2023-02-06 17:33:52 +0100

    update

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -17,10 +17,9 @@ function ProjectCards(props) {
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
+        
         {"\n"}
         {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
 
         {!props.isBlog && props.demoLink && (
           <Button
