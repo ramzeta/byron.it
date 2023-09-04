e1ba5046fbaeecbbd673364036421438a9966369
ramzeta <ramiroperezsanz13@gmail.com>
2023-09-04 17:40:35 +0200

    feat: add canvas

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,4 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+const { createCanvas } = require('canvas');
+window.HTMLCanvasElement.prototype.getContext = function (type) {
+  return createCanvas(0, 0).getContext(type);
+};
